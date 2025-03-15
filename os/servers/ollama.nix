a846b6e45f4d941cb6740523027daacd7002839f
Filip Brcic <brcha@yandex.com>
2025-03-16 00:11:24 +0100

    feat: update model list

--- os/servers/ollama.nix
+++ os/servers/ollama.nix
@@ -12,12 +12,14 @@ in
     home = "/persist/ollama";
     acceleration = "cuda";
     loadModels = [
-      "llama3.3"
+      #"llama3.3" # requires too much memory
       "llama3.2"
       "gemma3"
       "phi4"
       "deepseek-r1"
       "deepseek-coder-v2"
+      "qwen2.5-coder"
+      "qwen2.5"
     ];
   };
 }
