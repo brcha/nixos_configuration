e0db104527ca7d7a334b2141a520b03b0d421560
Filip Brcic <brcha@yandex.com>
2025-05-24 14:29:52 +0200

    feat: changed the channels to 25.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # NixOS Packages
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.11";
+      url = "github:nixos/nixpkgs/nixos-25.05";
     };
     nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixos-unstable";
@@ -21,7 +21,7 @@
 
     # Home manager (using unstable nixpkgs)
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
