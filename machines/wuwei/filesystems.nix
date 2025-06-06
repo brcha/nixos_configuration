bfd728a982cd76fae89ba5887530bce615869b08
Filip Brcic <brcha@yandex.com>
2025-06-06 20:17:54 +0200

    feat: remove kubelet fs

--- machines/wuwei/filesystems.nix
+++ machines/wuwei/filesystems.nix
@@ -54,11 +54,6 @@
     fsType = "zfs";
   };
 
-  fileSystems."/var/lib/kubelet" = {
-    device = "zroot/persistent/kubelet";
-    fsType = "zfs";
-  };
-
   fileSystems."/data" = {
     device = "ztank/data";
     fsType = "zfs";
