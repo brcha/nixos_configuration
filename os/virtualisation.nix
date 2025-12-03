d916d6959410d115b95a9f0556a6eb0ea361d24c
Filip Brcic <brcha@yandex.com>
2025-12-03 21:11:38 +0100

    feat: trim virtualisation a bit

--- os/virtualisation.nix
+++ os/virtualisation.nix
@@ -2,21 +2,10 @@
 
 {
   virtualisation = {
-    docker = {
-      enable = true;
-      autoPrune.enable = true; # Prune docker images (weekly by default)
-    };
-    podman = {
-      enable = true;
-      dockerCompat = false; # alias docker to podman
-    };
     virtualbox.host = {
       enable = true;
       enableExtensionPack = true;
     };
-    libvirtd = { enable = true; };
-    # anbox.enable = true; # BROKEN in 21.05
-    spiceUSBRedirection.enable = true;
     vmware.host = {
       enable = true;
     };
